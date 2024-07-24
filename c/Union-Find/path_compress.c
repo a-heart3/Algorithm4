92457aa47cf85dfa54bf5f148c171c96f00fbaab
ax <17302241931@163.com>
2024-07-24 21:11:02 +0800

    add path_compress

---
+++ c/Union-Find/path_compress.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+extern int *UF;
+extern int N;
+extern int *size;
+
+int find_root (int p)
+{
+	int root = p;
+	while (UF[root] != root) {
+		root = UF[root];
+	}
+	
+	/* Set the id[] of each examined node to the root */
+	while (UF[p] != p) {
+		UF[p] = root;
+		p = UF[p];
+	}
+
+	return p;
+}
+
+bool UF_connected (int p, int q)
+{
+	return (find_root(p) == find_root(q));
+}
+
+
+void UF_union (int p, int q)
+{
+	int root_p = find_root(p);
+	int root_q = find_root(q);
+	
+	/* use size to record the size of tree
+	 * make small tree as the large tree 's subtree
+	 */
+	if (size[root_p] >= size[root_q]) {
+		UF[root_q] = UF[root_p];
+		size[root_p] += size[root_q];
+	}
+	else {
+		UF[root_p] = UF[root_q];
+		size[root_q] += size[root_p];
+	}
+}
+
+int UF_count()
+{
+	int i = 0;
+	int count = 0;
+	for(i; i<N; i++) {
+		if (UF[i] == i) count++;
+	}
+
+	return count;
+}
+		
+
---
+++ c/Union-Find/path_compress2.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+extern int *UF;
+extern int N;
+extern int *size;
+
+int find_root (int p)
+{
+	/* Make every other node in path point to its grandparent */
+	while (UF[p] != p) {
+		UF[p] = UF[UF[p]];
+		p  = UF[p];
+	}
+	
+	return p;
+}
+
+bool UF_connected (int p, int q)
+{
+	return (find_root(p) == find_root(q));
+}
+
+
+void UF_union (int p, int q)
+{
+	int root_p = find_root(p);
+	int root_q = find_root(q);
+	
+	/* use size to record the size of tree
+	 * make small tree as the large tree 's subtree
+	 */
+	if (size[root_p] >= size[root_q]) {
+		UF[root_q] = UF[root_p];
+		size[root_p] += size[root_q];
+	}
+	else {
+		UF[root_p] = UF[root_q];
+		size[root_q] += size[root_p];
+	}
+}
+
+int UF_count()
+{
+	int i = 0;
+	int count = 0;
+	for(i; i<N; i++) {
+		if (UF[i] == i) count++;
+	}
+
+	return count;
+}
+		
+
